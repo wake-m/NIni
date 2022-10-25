4f8dc21a99b1fdc9c9f5cd27e12e4ac1b70458ee
wake-m <100056825+wake-m@users.noreply.github.com>
2022-10-25 13:58:00 +0800

    Add files via upload

--- Ini.cpp
+++ Ini.cpp
@@ -10,45 +10,93 @@ Ini::~Ini() {
 
 }
 
-key_value Ini::operator[](std::string s) {
-	
-	return this->all_data[s];
+key_value& Ini::operator[](std::string s) {
+	key_value* temp = &all_data[s];
+	return *temp;
 }
 
 
-void Ini::PointToFile(const std::string& filename) {
+bool Ini::PointToFile(const std::string& filename) {
 	std::ifstream file;
 	ConfigFile = filename;
-
 	file.open(this->ConfigFile,std::ifstream::in);
-	
-
 	if (file.is_open()) {
-		std::string section;
+		std::string session;
 		std::string line;
 		
-		while (std::getline(file,line))
-		{
-			int findcode = line.find('=');
+		while (std::getline(file,line)){
+			size_t findcode = line.find('=');
 			
 			if (line.empty()) {
-				section = "";
-			}else if (line.at(0) == '[') {
-				int end = line.find(']');
-				section = line.substr(1, end - 1);
+				session.clear();
+				continue;
+			}else if (line[0] == '[') {
+				session = line.substr(1, line.find(']') - 1);
+				continue;
 			}
 
 			if (findcode != line.npos) {
-				std::string key;
-				Value value;
-				key = line.substr(0, findcode);
-				value = line.substr(findcode+1, line.find('\n'));
-				this->all_data[section][key] = value;
+				all_data[session][line.substr(0, findcode)] = line.substr(findcode + 1, line.find('\n'));
+			}
+			else if (findcode == line.npos) {
+				close(); 
+				return false;
 			}
 			
 		}
+	}
+	return true;
+}
+
+
+
+
+void Ini::RefFile() {
+	std::fstream files;
+	// π”√∏≤∏«–¥»Î
+	files.open(GetConfigFile(),std::ios::out|std::ios::trunc);
 	
+	if (files.is_open()) {
+		for (auto& t : all_data) {
+			for (auto& ts : all_data[t.first]) {
+				if (t.first.empty()) {
+					files << ts.first << "=" << std::string(all_data[t.first][ts.first]) << "\n";
+				}
+			}	
+		}
+
+		std::vector<std::string> s;
+		
+		for (auto& t : all_data){
+			if (t.first == "") continue;
+			std::string temp = "[";
+			temp.append(t.first).append("]").append("\n");
+			for (auto& ts : all_data[t.first]) {
+				temp.append(ts.first).append("=").append(all_data[t.first][ts.first]).append("\n");
+			}
+			s.push_back(temp),temp.clear();
+		}
+		
+		for (std::string tm : s) {
+			files << tm << "\n";
+		}
 	}
 
-
+	files.close();
 }
+
+std::string Ini::GetConfigFile() {
+	return ConfigFile;
+}
+
+
+bool Ini::empty() {
+	return all_data.empty();
+}
+
+void Ini::close() {
+	this->ConfigFile.clear();
+	this->all_data.clear();
+}
+
+
--- Ini.h
+++ Ini.h
@@ -1,10 +1,17 @@
-#ifndef INI_H
-#define INI_H
 #include<iostream>
 #include<string>
 #include<fstream>
 #include<map>
 #include"Value.h"
+#include<ios>
+#include<vector>
+
+
+
+#ifndef INI_H
+#define INI_H
+
+
 
 typedef std::map<std::string, std::map<std::string, Value>> key_key_value;
 typedef std::map<std::string, Value> key_value;
@@ -15,15 +22,19 @@ class Ini
 public:
 	Ini();
 	~Ini();
-	void PointToFile(const std::string& filename);
-
-	key_value operator[](std::string s);
+	bool PointToFile(const std::string& filename);
+	void close();
+	bool empty();
+	std::string GetConfigFile();
+	key_value& operator[](std::string s);
+	void RefFile();
 private:
+	
 	std::string ConfigFile;
 	key_key_value all_data;
 
 };
-#endif // !INI_H
+#endif // !INI_Hs
 
 
 
